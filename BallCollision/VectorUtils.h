f71cc5fc2681e784def7bf558d7edabe9c6e563a
stchern <nastyachernyshovabbk00@mail.ru>
2022-12-09 13:54:25 +0300

    Add almostEqual funs for vectors comparison

--- BallCollision/VectorUtils.h
+++ BallCollision/VectorUtils.h
@@ -17,6 +17,13 @@ T dot(const sf::Vector2<T>& lhsV, const sf::Vector2<T>& rhsV) {
     return (lhsV.x * rhsV.x + lhsV.y * rhsV.y);
 }
 
+template <typename T>
+bool isAlmostEqual(const sf::Vector2<T>& lhsV, const sf::Vector2<T>& rhsV) {
+    const T epsilon = 1e-6;
+    return (std::abs(lhsV.x - rhsV.x) <= epsilon && std::abs(lhsV.y - rhsV.y) <= epsilon);
+}
+
+
 };
 
 #endif // VECTORUTILS_H
